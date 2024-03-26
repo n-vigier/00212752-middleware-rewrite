00ed9d8537a13572520d4d3cab1406c2ae923ca9
Nick Vigier <nvigier46@gmail.com>
2024-03-26 12:24:48 +0000

    added logging

--- middleware.ts
+++ middleware.ts
@@ -4,22 +4,30 @@ export default function middleware(request: Request) {
   const url = new URL(request.url)
   const hostname = request.headers.get('host')
   const path = url.pathname
+  console.log({url, hostname, path});
+  
 
   if (hostname === 'docs.nicolas-vigier.com') {
     console.log(`Rewriting /docs${path}`)
     console.log(`URL: ${new URL(`/docs${path}`, request.url)}`)
     return rewrite(new URL(`/docs${path}`, request.url))
   }
+  console.log('Docs not matched');
+  
 
   if (hostname === 'careers.nicolas-vigier.com') {
     console.log(`Rewriting /careers${path}`)
     console.log(`URL: ${new URL(`/careers${path}`, request.url)}`)
     return rewrite(new URL(`/careers${path}`, request.url))
   }
+  console.log('Careers not matched');
 
   if (hostname === 'blog.nicolas-vigier.com') {
     console.log(`Rewriting /blog${path}`)
     console.log(`URL: ${new URL(`/blog${path}`, request.url)}`)
     return rewrite(new URL(`/blog${path}`, request.url))
   }
+
+  console.log('Blog not matched');
+
 }
