c2bee57d8aeaa1d26981d2004aa6a5de7c97b31e
Nick Vigier <nvigier46@gmail.com>
2024-03-26 12:03:00 +0000

    update rewrite domain

--- middleware.ts
+++ middleware.ts
@@ -5,21 +5,21 @@ export default function middleware(request: Request) {
   const hostname = request.headers.get('host')
   const path = url.pathname
 
-  if (hostname === 'docs.testdomain.co') {
+  if (hostname === 'docs.nicolas-vigier.com') {
     console.log(`Rewriting /docs${path}`)
     console.log(`URL: ${new URL(`/docs${path}`, request.url)}`)
     return rewrite(new URL(`/docs${path}`, request.url))
   }
 
-  if (hostname === 'careers.testdomain.co') {
+  if (hostname === 'careers.nicolas-vigier.com') {
     console.log(`Rewriting /careers${path}`)
-    console.log(`URL: ${new URL(`/docs${path}`, request.url)}`)
+    console.log(`URL: ${new URL(`/careers${path}`, request.url)}`)
     return rewrite(new URL(`/careers${path}`, request.url))
   }
 
-  if (hostname === 'blog.testdomain.co') {
+  if (hostname === 'blog.nicolas-vigier.com') {
     console.log(`Rewriting /blog${path}`)
-    console.log(`URL: ${new URL(`/docs${path}`, request.url)}`)
+    console.log(`URL: ${new URL(`/blog${path}`, request.url)}`)
     return rewrite(new URL(`/blog${path}`, request.url))
   }
 }
