064eb2b2ad2844450d6a259187db2df71172daca
Nick Vigier <nvigier46@gmail.com>
2024-03-28 08:14:24 +0000

    Use default custom domain to rewrite

--- middleware.ts
+++ middleware.ts
@@ -4,34 +4,29 @@ export default function middleware(request: Request) {
   const url = new URL(request.url);
   const hostname = request.headers.get('host');
   const path = url.pathname;
-  console.log({ url: JSON.stringify(url, null, 2), hostname, path });
-  console.log(process.env);
-  
+
+  url.hostname = 'rewrite.nicolas-vigier.com';
 
   if (hostname === 'docs.nicolas-vigier.com') {
     console.log(`Rewriting /docs${path}`);
-    url.hostname = '00212752-middleware-rewrite.vercel.app';
-    console.log(`URL: ${new URL(`/docs${path}`, url)}`);
-    return rewrite(new URL(`/docs${path}`, url));
+    url.pathname = `/docs${path}`;
+    console.log(`URL: ${url.href}`);
+    return rewrite(url);
   }
-  console.log('Docs not matched');
 
   if (hostname === 'careers.nicolas-vigier.com') {
     console.log(`Rewriting /careers${path}`);
-    url.hostname = '00212752-middleware-rewrite.vercel.app';
-    console.log(`URL: ${new URL(`/careers${path}`, url)}`);
-    return rewrite(new URL(`/careers${path}`, url));
+    url.pathname = `/careers${path}`;
+    console.log(`URL: ${url.href}`);
+    return rewrite(url);
   }
-  console.log('Careers not matched');
 
   if (hostname === 'blog.nicolas-vigier.com') {
     console.log(`Rewriting /blog${path}`);
-    url.hostname = '00212752-middleware-rewrite.vercel.app';
-    console.log(`URL: ${new URL(`/blog${path}`, url)}`);
-    return rewrite(new URL(`/blog${path}`, url));
+    url.pathname = `/blog${path}`;
+    console.log(`URL: ${url.href}`);
+    return rewrite(url);
   }
-
-  console.log('Blog not matched');
 }
 
 export const config = {
